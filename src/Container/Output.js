455d31d98091584006b49caa7cb8f64bf6e5a1cc
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-07 21:41:57 +0300

    add and refactor  react-syntax-highlighter

--- src/Container/Output.js
+++ src/Container/Output.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Header from './Header'
 import ReactMarkDown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function Output() {
     const [input, setInput] = useState();
@@ -18,6 +20,24 @@ function Output() {
             <ReactMarkDown 
                 className="output__box" 
                 children={input} 
+                components={{
+                    code({node, inline, className, children, ...props}) {
+                      const match = /language-(\w+)/.exec(className || '')
+                      return !inline && match ? (
+                        <SyntaxHighlighter
+                          children={String(children).replace(/\n$/, '')}
+                          style={docco}
+                          language={match[1]}
+                          PreTag="div"
+                          {...props}
+                        />
+                      ) : (
+                        <code className={className} {...props}>
+                          {children}
+                        </code>
+                      )
+                    }
+                  }}
             />
         </div>
         </>
